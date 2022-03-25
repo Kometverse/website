3a654f72ebd8594f7a857f67c98f1650ec2ca27b
vikash <yes.vikash.me@gmail.com>
2022-03-26 01:02:20 +0530

    firebase added

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,112 @@
-import { useState } from "react"
-import { CTA } from "./componets/bottomCta"
-import { Features } from "./componets/Features"
-import { Footer } from "./componets/footer"
-import { Hero } from "./componets/Hero"
-import { Intro } from "./componets/Intro"
-import { Navbar } from "./componets/Navbar"
-function App() {
+import { useEffect, useState } from "react";
+import { CTA } from "./componets/bottomCta";
+import { Features } from "./componets/Features";
+import { Footer } from "./componets/footer";
+import { Hero } from "./componets/Hero";
+import { Intro } from "./componets/Intro";
+import { Navbar } from "./componets/Navbar";
+import { v4 as uuidv4 } from "uuid";
 
-  const [email, setEmail] = useState("");
+import {
+  collection,
+  getDoc,
+  doc,
+  updateDoc,
+  getDocs,
+  addDoc,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "./firebase";
+
+const queryParams = new URLSearchParams(window.location.search);
+const refralurl = queryParams.get("refID");
+console.log(refralurl);
+
+function App() {
+  const useresref = collection(db, "users");
+  const [email, setEmail] = useState("yes.vikass.med@outlook.com");
   const [submit, setSubmit] = useState(false);
-  const [isDisabled, setDisabled] = useState(true)
+  const [isDisabled, setDisabled] = useState(true);
+  const [newUser, setNewUser] = useState({});
   const handleChnage = (e: any) => {
-    setEmail(e.target.value)
+    setEmail(e.target.value);
     if (email.length > 3) {
-      setDisabled(false)
-      console.log("you can click now ")
+      console.log("you can click now ");
     }
-    setDisabled(true)
-  }
+  };
   const onClick = (e: any) => {
-    setSubmit(!submit)
-    console.log("hello")
-  }
+    setSubmit(!submit);
+    console.log("hello");
+  };
+
+  const createNewUser = async () => {
+    const newrefID = uuidv4();
+    console.log(newrefID);
+    const newuserData = {
+      email: email,
+      points: 0,
+      refID: newrefID,
+    };
+    await addDoc(useresref, newuserData);
+    // get user from db
+    setNewUser(() => {
+      return { ...newuserData };
+    });
+    console.log(newUser);
+  };
+
+  useEffect(() => {
+    (async () => {
+      // rrab users db refrance
+      // user email query
+      const emailQuery = query(useresref, where("email", "==", email));
+      const usersExist = (await getDocs(emailQuery)).docs.length;
+      // check if emal that was entred exists or not
+      if (usersExist) {
+        console.log("users exists");
+      } else {
+        // if not then add that user to db and then create a refral id for that user retturn that url to frontend
+        if (!refralurl) {
+          createNewUser();
+        } else {
+          const userq = query(useresref, where("refID", "==", refralurl));
+          const refralUser = await getDocs(userq);
+          if (!refralUser.empty) {
+            // update user point and create nw user
+            const refralUserDaata = refralUser.docs[0].data();
+            const docref = doc(useresref, refralUser.docs[0].id);
+            updateDoc(docref, {
+              points: refralUserDaata.points + 1,
+            });
+
+            createNewUser();
+          } else {
+          }
+        }
+      }
+    })();
+  }, []);
+
   return (
-    // top level container 
+    // top level container
     <div className="">
       <div className="text-white relative w-full min-h-screen bg-primary backdrop-blur-3xl ">
         <div className="xl:w-[80%] m-auto ">
           <Navbar />
-          <Hero onClick={onClick} isdisabled={isDisabled} handleChnage={handleChnage} />
+          <Hero
+            onClick={onClick}
+            isdisabled={isDisabled}
+            handleChnage={handleChnage}
+          />
           <Intro />
           <Features />
           <CTA />
           <Footer />
         </div>
-      </div >
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componets/Hero.tsx
+++ src/componets/Hero.tsx
@@ -1,5 +1,3 @@
-import joinus from "/joinus.svg"
-
 interface Props {
     handleChnage: any
     isdisabled: boolean
@@ -9,20 +7,17 @@ interface Props {
 
 
 export const Hero = (props: Props) => {
-    return (
+         return (
         <>
             <div className="flex flex-col items-center text-center w-[90%] mt-5   pb-6 rounded-2xl pt-8  backdrop-blur relative bg-[#2A2A3C] sm:max-w-[600px] m-auto sm:pt-20 sm:pb-20 lg:max-w-[700px] sm:mt-20 lg:rounded-[90px]  lg:mb-20 sm:mb-10 ">
                 <div className="select-none blur-2xl w-56 h-56 -top-10 -z-19 left-0 absolute bg-[#8146FF]/50 rounded-full"></div>
                 <h1 className=" pb-2 text-center text-4xl sm:text-5xl lg:text-7xl">KOMET</h1>
                 <p className="text-lg mb-2 sm:text-2xl lg:text-3xl  lg:mb-8">The fastest and esiest web3 access</p>
                 <div className="flex flex-col w-full  items-center sm:flex-row sm:bg-white sm:w-[60%] sm:mt-5 sm:justify-between sm:rounded-full">
-                    <input type="text" placeholder="Email Adresss" className="px-2 mb-4 rounded-full  sm:px-3 sm:py-1 w-[180px] sm:m-0  text-black" onChange={props.handleChnage} />
+                    <input type="text" placeholder="Email Adresss" className="px-2 mb-4 focus-within:outline-none rounded-full  sm:px-3 sm:py-1 w-[180px] sm:m-0  text-black" onChange={props.handleChnage} />
                     <button disabled={props.isdisabled} className="text-center px-5 rounded-full top-btn py-2 bg-slate-500   " onClick={props.onClick}>join wishlist </button>
                 </div>
-
             </div>
-
-
         </>
     )
 }
