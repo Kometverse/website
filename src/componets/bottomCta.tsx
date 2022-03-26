402a7937fec11bd0cc5fc697c1041f1e13b45ee3
= <=>
2022-03-26 11:44:16 +0530

    firebase added

--- src/componets/bottomCta.tsx
+++ src/componets/bottomCta.tsx
@@ -1,66 +1,69 @@
 interface Props {
-    setEmail: any
+
+  setEmail: any
 }
- import { useFormik } from 'formik';
+import { useFormik } from 'formik';
 import { useEffect, useState } from 'react';
 
-export const CTA = ({setEmail} :Props) => {
-  const [value, setValue] = useState("") 
+export const CTA = ({ setEmail }: Props) => {
+  const [value, setValue] = useState("")
   const [isDissabled, setIsdisabled] = useState(true)
-    const emailRegex = /\S+@\S+\.\S+/;
-   const [isValid, setIsValid] = useState(false);
+  const emailRegex = /\S+@\S+\.\S+/;
+  const [isValid, setIsValid] = useState(false);
   const [message, setMessage] = useState('');
 
   const btnStyles = `${isDissabled ? "cursor-not-allowed" : "pointer"}`
- const handleChange = (e:any) =>{
+  const handleChange = (e: any) => {
     setValue(e.target.value)
-    
- }  
 
- const handleSubmit = (e:any) => {
-   e.preventDefault();
-   setEmail(value)
-   console.log("yeah ")
+  }
 
- }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setEmail(value)
+    console.log("yeah ")
+    setValue("")
 
- useEffect(() => {
-     if(value.length) {
-    if (emailRegex.test(value)) {
-      setIsValid(true);
-      setMessage('Your email looks good!');
-      setIsdisabled(false)
+  }
+
+  useEffect(() => {
+    if (value.length) {
+      if (emailRegex.test(value)) {
+        setIsValid(true);
+        setMessage('Your email looks good!');
+        setIsdisabled(false)
+      } else {
+        setIsValid(false);
+        setMessage("your email is not valid")
+        setIsdisabled(true)
+      }
     } else {
-      setIsValid(false);
-      setMessage("your email is not valid")
-      setIsdisabled(true)
+      setMessage("")
     }
-     }else {
-       setMessage("")
-     }
-   
-   }, [value])
+
+  }, [value])
 
 
-   return (
-     <>
-  <div className='w-full  bg-red-500 lg:py-32 py-12  flex flex-col items-center'>
-    <h1 className='text-3xl mb-10'>CLAIM EARLY Access</h1>
-      <form  className=' w-full m-auto ' onSubmit={handleSubmit}>
-        <div className='border-black border rounded-full lg:w-[70%] m-auto flex flex-row w-[90%] bg-white '>
-       <input
-         id="email"
-         name="email"
-         type="email"
-         onChange={handleChange}
-         value={value}
-         className="text-black w-full rounded-full"
-       />
-       <button className= {btnStyles + " text-black text-sm  rounded-full bg-blue-500 px-3 lg:py-3  py-1 lg:text-3xl"} disabled={isDissabled} type="submit">Submit</button>
-        </div>
-     <p className='text-white'>{isValid ? message : message}</p>  
-     </form>
-  </div>
-     </>
-   );
+  return (
+    <>
+      <div className='w-full cta lg:py-32 py-20  flex flex-col items-center '>
+        <h1 className='text-3xl sm:text-5xl font-bold mb-10 lg:text-7xl '>claim early Access</h1>
+        <form className=' w-full m-auto ' onSubmit={handleSubmit}>
+          <div className=' border rounded-full  lg:w-[60%] m-auto flex flex-row w-[90%] bg-white '>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={handleChange}
+              value={value}
+              placeholder="Enter your email"
+              className="text-black grow rounded-full px-[5px] lg:px-4 text-sm outline-none sm:text-xl"
+            />
+            <button className={btnStyles + " text-sm sm:text-lg  rounded-full bg-blue-500  lg:py-3 px-4  lg:px-6 py-2 lg:text-3xl"} disabled={isDissabled} type="submit">join waitlist</button>
+          </div>
+          <p className='text-white text-center lg:text-xl' >{isValid ? message : message}</p>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/componets/footer.tsx
+++ src/componets/footer.tsx
@@ -3,17 +3,17 @@ import twitter from "/twitter.svg"
 export const Footer = () => {
     return (
         <>
-       <div className=" mt-10 py-5 flex items-center justify-around text-sm px-2 lg:text-xl">
-       <h1>KOMET</h1>    
-       <p>made with <i className="text-red-500">❤️</i> from india for global  </p>
-       <div className="flex">
-        <img className="mx-3" src={discord} alt="" />
-        <img src={twitter} alt="" />
-       </div>
-        </div> 
-        <div className= "mb-12 text-center">
-            <p>&copy; 2022 KOMET | all rights reserved 	</p>
-        </div>
+            <div className=" mt-10 py-5 flex items-center justify-around text-sm px-2 lg:text-xl">
+                <h1>KOMET</h1>
+                <p>made with <i className="text-red-500">❤️</i> from india for global  </p>
+                <div className="flex">
+                    <img className="mx-3 w-5 lg:w-10" src={discord} alt="" />
+                    <img src={twitter} alt="" className="w-5 lg:w-10" />
+                </div>
+            </div>
+            <div className="mb-12 text-center">
+                <p>&copy; 2022 KOMET | all rights reserved 	</p>
+            </div>
         </>
     )
 }
