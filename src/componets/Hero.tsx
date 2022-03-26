8ee7a246b125e1cd7348289bdc4abf0b6ec7149d
vikash <yes.vikash.me@gmail.com>
2022-03-26 22:47:03 +0530

    Tilt added

--- src/componets/Hero.tsx
+++ src/componets/Hero.tsx
@@ -3,7 +3,7 @@ import emoji from "/emoji.svg"
 export const Hero = () => {
     return (
         <>
-            <div className="w-full flex lg:my-32  text=center flex-col items-center ">
+            <div className="w-full lg:h-[90vh] border flex  flex-col items-center justify-center">
                 <h1 className="text-4xl sm:text-6xl font-bold lg:text-8xl mb-4 lg:mb-8 ">It's your gateway</h1>
                 <p className="text-4xl sm:text-6xl font-bold lg:text-8xl mb-2 web3-text lg:mb-8 ">to web3</p>
                 <p className="flex text-xl lg:text-2xl mb-4">send swap and earn - like a pro <img className=" mx-2 w-5" src={emoji} alt="" /> </p>
--- src/componets/titlcard.tsx
+++ src/componets/titlcard.tsx
@@ -1,33 +1,65 @@
-import bitcoin from "/bitcoin.svg"
-import goal from "/gals.svg"
-import future from "/future.svg"
+import bitcoin from "/bitcoin.svg";
+import goal from "/gals.svg";
+import future from "/future.svg";
+import Tilt from "react-tilt";
 
 export const TiltCards = () => {
-    return (
-        <>
+  return (
+    <>
+      <p className="px-5  text-center max-w-[35ch] py-5 text-3xl bold mb-4  sm:text-7xl lg:py-10 font-bold m-auto ">
+        {" "}
+        Unlocking your digital world
+      </p>
+      <div className="lg:w-[90%] relative  flex-col flex lg:items-center lg:flex-row space-between  m-auto my-15 sm:mb-28 lg:mb-32">
+       
+        <Tilt
+          options={{ max: 25 }}
+         className="Tilt lg:w-[31%] w-[95%] m-auto opacity-80 hover:opacity-100 lg:mx-5 max-w-lg card rounded-xl relative  min-h-[300px] my-4  ">
+          <div className="Tilt-inner ">
+           <p className="text-4xl px-6 py-7 opacity-90">Secure Transections</p>
+          <p className="text-xl text-left w-[15ch] px-6 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
+          </p>
+          <img
+            src={bitcoin}
+            className="w-36 absolute right-3 bottom-3"
+            alt=""
+          />   
+          </div>
+        </Tilt>
+          <Tilt
+          options={{ max: 25 }}
+         className="Tilt lg:w-[31%] w-[95%] m-auto opacity-80 hover:opacity-100 lg:mx-5 max-w-lg card rounded-xl relative  min-h-[300px] my-4  ">
+          <div className="Tilt-inner ">
+           <p className="text-4xl px-6 py-7 opacity-90">Secure Transections</p>
+          <p className="text-xl text-left w-[15ch] px-6 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
+          </p>
+          <img
+            src={bitcoin}
+            className="w-36 absolute right-3 bottom-3"
+            alt=""
+          />   
+          </div>
+        </Tilt>
+          <Tilt
+          options={{ max: 25 }}
+         className="Tilt lg:w-[31%] w-[95%] m-auto opacity-80 hover:opacity-100 lg:mx-5 max-w-lg card rounded-xl relative  min-h-[300px] my-4  ">
+          <div className="Tilt-inner ">
+           <p className="text-4xl px-6 py-7 opacity-90">Secure Transections</p>
+          <p className="text-xl text-left w-[15ch] px-6 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. .
+          </p>
+          <img
+            src={bitcoin}
+            className="w-36 absolute right-3 bottom-3"
+            alt=""
+          />   
+          </div>
+        </Tilt>
 
-            <p className="px-5  text-center max-w-[35ch] py-5 text-3xl bold mb-4  sm:text-7xl lg:py-10 font-bold m-auto "> Unlocking your digital world</p>
-            <div className="lg:w-[90%] relative  flex-col flex lg:items-center lg:flex-row space-between  m-auto my-15 sm:mb-28 lg:mb-32" >
-
-                <div className="lg:w-[31%] w-[95%] m-auto opacity-80 hover:opacity-100 transition-opacity lg:mx-5 max-w-lg card rounded-xl relative  min-h-[300px] my-4  ">
-                    <p className="text-4xl px-6 py-7 opacity-90">Secure Transections</p>
-                    <p className="text-xl text-left w-[15ch] px-6 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. .</p>
-                    <img src={bitcoin} className="w-36 absolute right-3 bottom-3" alt="" />
-                </div>
-                <div className="lg:w-[31%] w-[95%] m-auto opacity-80 hover:opacity-100 lg:mx-5 max-w-lg card rounded-xl relative  min-h-[300px] my-4  ">
-                    <p className="text-4xl px-6 py-7 opacity-90">Secure Transections</p>
-                    <p className="text-xl text-left w-[15ch] px-6 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. .</p>
-                    <img src={bitcoin} className="w-36 absolute right-3 bottom-3" alt="" />
-                </div>
-                <div className="lg:w-[31%] w-[95%] m-auto opacity-80 hover:opacity-100 lg:mx-5 max-w-lg card rounded-xl relative  min-h-[300px] my-4  ">
-                    <p className="text-4xl px-6 py-7 opacity-90">Secure Transections</p>
-                    <p className="text-xl text-left w-[15ch] px-6 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. .</p>
-                    <img src={bitcoin} className="w-36 absolute right-3 bottom-3" alt="" />
-                </div>
-
-
-
-            </div>
-        </>
-    )
-}
+    
+      </div>
+    </>
+  );
+};
