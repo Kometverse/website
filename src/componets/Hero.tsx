a9d7c5c95a736461a18f6e9238cc47fea5ce8c59
vikash <yes.vikash.me@gmail.com>
2022-03-26 12:16:14 +0530

    hero section changed

--- src/componets/Hero.tsx
+++ src/componets/Hero.tsx
@@ -1,10 +1,14 @@
+import join from "/join.svg"
+
+
 export const Hero = () => {
     return (
         <>
-            <div className="flex flex-col items-center text-center w-[90%] mt-5   pb-6 rounded-2xl pt-8  backdrop-blur relative bg-[#2A2A3C] sm:max-w-[600px] m-auto sm:pt-20 sm:pb-20 lg:max-w-[700px] sm:mt-20 lg:rounded-[90px]  lg:mb-20 sm:mb-10 ">
-                <div className="select-none blur-2xl w-56 h-56 -top-10 -z-19 left-0 absolute bg-[#8146FF]/50 rounded-full"></div>
-                <h1 className=" pb-2 text-center text-4xl sm:text-5xl lg:text-7xl">KOMET</h1>
-                <p className="text-lg mb-2 sm:text-2xl lg:text-3xl  lg:mb-8">The fastest and esiest web3 access</p>
+            <div className="w-full">
+                <h1>it's your gateway</h1>
+                <p>to web3</p>
+                <p>send swap and earn - like a pro</p>
+                <button>join waitlist -> </button>
             </div>
         </>
     )
