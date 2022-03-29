f4475a2cf5f01a134de6685fc9af48ce3d474957
= <=>
2022-03-29 14:17:52 +0530

    images bug fixed

--- src/componets/Rank.tsx
+++ src/componets/Rank.tsx
@@ -1,4 +1,4 @@
-import astro from "/ast.svg";
+import ast from "/ast.svg";
 import future from "/future.svg";
 import { db } from "../firebase";
 import { collection, getDocs, query, where, orderBy } from "firebase/firestore";
@@ -22,7 +22,6 @@ export const Rank = ({ user }: Props) => {
   const [rank, setRank] = useState(0);
   const [reflink, setrefLink] = useState("");
   const [isCopied, setisCopied] = useState(false);
-
 
 
   useEffect(() => {
@@ -79,38 +78,36 @@ export const Rank = ({ user }: Props) => {
         />
         <div className="select-none xl:block blur-2xl lg:w-48 w-16 h-16 lg:h-40  hidden lg:-top-20 lg:-right-20 absolute bg-[#8146FF]/50 rounded-full"></div>
         <div className="select-none xl:block blur-2xl lg:w-48 w-16 h-16 lg:h-40  hidden lg:-bottom-20  lg:-left-20  absolute bg-[#8146FF]/50 rounded-full"></div>
-        <div className="flex flex-col justify-center items-center">
-          <img src={astro} className="w-44 lg:w-48" alt="" />
-          <h2 className="flex text-lg leading-6 py-4 lg:text-2xl">
-            That's Cool <img src={future} className=" mx-2 w-6" alt="" />
-          </h2>
-          <p className="rankjoin font-bold text-xl">
-            Thanks for joining the waitlist!
-          </p>
-          <p className="p-5"> Your Current Postion</p>
-          <div className="border border-[#8146FF] w-[90%] text-center text-xl p-2 max-w-md">
-            # {rank ? rank : "loading....."}
-          </div>
 
-          <p className="text-sm my-6 text-center px-8">
-            The first 1000 frens can get an exclusive NFT airdrop of worth $100 🎉
-          </p>
-          <div className="flex justify-between  w-[90%] text-sm max-w-md ">
-            <RWebShare
-              data={{
-                text: "",
-                url: `https://komet-xi.vercel.app/?refID=${user.refID}`,
-                title: "Invite freinds",
-              }}
-
-              onClick={() => setisCopied(true)}
-            >
-              <button className="w-full intro-gradient rounded-lg p-2 text-xl  ">Invite Friends</button>
-            </RWebShare>
-          </div>
-          <p className="lg:block hidden text-green-500"> {isCopied ? "link  copied" : ""}</p>
-
+        <img src={ast} className="w-44 lg:w-48" alt="" />
+        <h2 className="flex text-lg leading-6 py-4 lg:text-2xl">
+          That's Cool <img src={future} className=" mx-2 w-6" alt="" />
+        </h2>
+        <p className="rankjoin font-bold text-xl">
+          Thanks for joining the waitlist!
+        </p>
+        <p className="p-5"> Your Current Postion</p>
+        <div className="border border-[#8146FF] w-[90%] text-center text-xl p-2 max-w-md">
+          # {rank ? rank : "loading....."}
         </div>
+
+        <p className="text-sm my-6 text-center px-8">
+          The first 1000 frens can get an exclusive NFT airdrop of worth $100 🎉
+        </p>
+        <div className="flex justify-between  w-[90%] text-sm max-w-md ">
+          <RWebShare
+            data={{
+              text: "",
+              url: `https://komet-xi.vercel.app/?refID=${user.refID}`,
+              title: "Invite freinds",
+            }}
+
+            onClick={() => setisCopied(true)}
+          >
+            <button className="w-full intro-gradient rounded-lg p-2 text-xl  ">Invite Friends</button>
+          </RWebShare>
+        </div>
+        <p className="lg:block hidden text-green-500"> {isCopied ? "link  copied" : ""}</p>
       </div>
 
     </>
