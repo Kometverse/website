86184254c6172b2410903830b92bfbf91d5e3a91
= <=>
2022-03-29 14:20:25 +0530

    images bug fixed

--- src/componets/Rank.tsx
+++ src/componets/Rank.tsx
@@ -79,7 +79,7 @@ export const Rank = ({ user }: Props) => {
         <div className="select-none xl:block blur-2xl lg:w-48 w-16 h-16 lg:h-40  hidden lg:-top-20 lg:-right-20 absolute bg-[#8146FF]/50 rounded-full"></div>
         <div className="select-none xl:block blur-2xl lg:w-48 w-16 h-16 lg:h-40  hidden lg:-bottom-20  lg:-left-20  absolute bg-[#8146FF]/50 rounded-full"></div>
 
-        <img src={ast} className="w-44 lg:w-48" alt="" />
+        <img src={ast} className="w-44 h-44 lg:w-48" alt="" />
         <h2 className="flex text-lg leading-6 py-4 lg:text-2xl">
           That's Cool <img src={future} className=" mx-2 w-6" alt="" />
         </h2>
